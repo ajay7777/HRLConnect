62653806c765da0523c55a9704bedacea405ff9f
k.chandra.mandal <kcmandal94@gmail.com>
2017-05-19 15:03:59 +0530

    Modified pages

---
+++ HRLConnect/BAL/SessionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HRLConnect.DAL;
+using System.Data;
+
+namespace HRLConnect.BAL
+{
+    public class SessionService
+    {
+        public static DataSet getUserDetails()
+        {
+            DataSet ds = new DataSet();
+            ds= SessionDAL.
+            return ds;
+        }
+    }
+    
+}
